008b4429ce27eab78d5292c23cd368a5fd0320ae
Sergey1234 <89917779+SergeyBelolipetskiy1234@users.noreply.github.com>
2021-09-03 22:58:45 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,43 @@
 package jm.task.core.jdbc.util;
 
+
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static Connection connection;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/new_schema?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+
+
+    // Driver driver;
+
+    public static Connection getConnection() {
+
+
+        {
+            try {
+                Class.forName(DRIVER);
+                // DriverManager.registerDriver(driver);
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
+            } /* finally {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } */
+            return connection;
+        }
+
+    }
 }
